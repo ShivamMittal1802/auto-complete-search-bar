e6560399b1bd53170323256274263a18d18bdb7a
Shivam Mittal <shivammittal@MacBookAir-844.lan>
2025-03-06 13:01:22 +0530

    feat: autoComplete search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState([]);
+  const [isExpand, setIsExpand] = useState(false);
+  const [cached, setCached] = useState({});
+
+  useEffect(() => {
+    console.log("api calls ", input);
+    const timer = setTimeout(fetchData, 300); 
+    return () => clearTimeout(timer);
+  }, [input]);
+
+  const fetchData = async () => {
+    if(cached[input]){
+      console.log("cached API: ", input)
+      setResult(cached[input]);
+      return ;
+    }
+    const data = await fetch("https://dummyjson.com/recipes/search?q=" + input);
+    const json = await data.json();
+    setResult(json?.recipes);
+    setCached((prev)=>({...prev, [input]: json?.recipes}));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Auto-Complete Search Bar</h1>
+      <div>
+        <input
+          type="text"
+          className="input-container"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onFocus={() => setIsExpand(true)}
+          onBlur={() => setIsExpand(false)}
+        />
+        {isExpand && (
+          <div className="result-space">
+            {result.map((item) => {
+              return (
+                <span className="result" key={item.id}>
+                  {item.name}
+                </span>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
